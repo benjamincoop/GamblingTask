eac3e3cc3063be6aa4badc6a7ee80899db0fd426
Benjamin Cooper <benjaminc@ksu.edu>
2021-01-04 23:26:40 -0600

    Created GUI for configuring trial phases. Minor changes to main GUI.

--- Gambling Task/Form1.Designer.cs
+++ Gambling Task/Form1.Designer.cs
@@ -30,62 +30,44 @@
         {
             this.components = new System.ComponentModel.Container();
             this.MenuStrip = new System.Windows.Forms.MenuStrip();
-            this.MenuActions = new System.Windows.Forms.ToolStripMenuItem();
-            this.MenuActionsStart = new System.Windows.Forms.ToolStripMenuItem();
-            this.MenuActionsStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuConfig = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.RollButton = new System.Windows.Forms.Button();
-            this.CollectButton = new System.Windows.Forms.Button();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
             this.Slot1 = new System.Windows.Forms.Button();
             this.Slot2 = new System.Windows.Forms.Button();
             this.Slot3 = new System.Windows.Forms.Button();
-            this.SlotBlinkTimer = new System.Windows.Forms.Timer(this.components);
+            this.BlinkTimer = new System.Windows.Forms.Timer(this.components);
             this.TimeoutTimer = new System.Windows.Forms.Timer(this.components);
+            this.CenterButton = new System.Windows.Forms.Button();
+            this.MenuFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileLoad = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuStrip.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // MenuStrip
             // 
             this.MenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.MenuActions,
+            this.MenuFile,
             this.MenuConfig,
             this.MenuHelp,
             this.MenuExit});
             this.MenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip.Name = "MenuStrip";
-            this.MenuStrip.Size = new System.Drawing.Size(1738, 28);
+            this.MenuStrip.Size = new System.Drawing.Size(2176, 28);
             this.MenuStrip.TabIndex = 0;
             // 
-            // MenuActions
+            // MenuFile
             // 
-            this.MenuActions.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.MenuActionsStart,
-            this.MenuActionsStop});
-            this.MenuActions.Enabled = false;
-            this.MenuActions.Name = "MenuActions";
-            this.MenuActions.Size = new System.Drawing.Size(70, 24);
-            this.MenuActions.Text = "Actions";
-            // 
-            // MenuActionsStart
-            // 
-            this.MenuActionsStart.Enabled = false;
-            this.MenuActionsStart.Name = "MenuActionsStart";
-            this.MenuActionsStart.Size = new System.Drawing.Size(115, 26);
-            this.MenuActionsStart.Text = "Start";
-            this.MenuActionsStart.Click += new System.EventHandler(this.MenuActionsStart_Click);
-            // 
-            // MenuActionsStop
-            // 
-            this.MenuActionsStop.Enabled = false;
-            this.MenuActionsStop.Name = "MenuActionsStop";
-            this.MenuActionsStop.Size = new System.Drawing.Size(115, 26);
-            this.MenuActionsStop.Text = "Stop";
-            this.MenuActionsStop.Click += new System.EventHandler(this.MenuActionsStop_Click);
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFileNew,
+            this.MenuFileLoad});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(44, 24);
+            this.MenuFile.Text = "File";
             // 
             // MenuConfig
             // 
@@ -108,43 +90,45 @@
             this.MenuExit.Text = "Exit";
             this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
             // 
-            // RollButton
+            // LeftButton
             // 
-            this.RollButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.RollButton.AutoSize = true;
-            this.RollButton.BackColor = System.Drawing.Color.Red;
-            this.RollButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RollButton.ForeColor = System.Drawing.Color.Black;
-            this.RollButton.Location = new System.Drawing.Point(329, 637);
-            this.RollButton.Name = "RollButton";
-            this.RollButton.Size = new System.Drawing.Size(500, 250);
-            this.RollButton.TabIndex = 1;
-            this.RollButton.Text = "Roll";
-            this.RollButton.UseVisualStyleBackColor = false;
-            this.RollButton.Click += new System.EventHandler(this.RollButton_Click);
+            this.LeftButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftButton.AutoSize = true;
+            this.LeftButton.BackColor = System.Drawing.Color.Gray;
+            this.LeftButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeftButton.ForeColor = System.Drawing.Color.DimGray;
+            this.LeftButton.Location = new System.Drawing.Point(278, 835);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(500, 250);
+            this.LeftButton.TabIndex = 1;
+            this.LeftButton.Text = "Roll";
+            this.LeftButton.UseVisualStyleBackColor = false;
             // 
-            // CollectButton
+            // RightButton
             // 
-            this.CollectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.CollectButton.AutoSize = true;
-            this.CollectButton.BackColor = System.Drawing.Color.Gray;
-            this.CollectButton.Enabled = false;
-            this.CollectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CollectButton.ForeColor = System.Drawing.Color.DimGray;
-            this.CollectButton.Location = new System.Drawing.Point(912, 637);
-            this.CollectButton.Name = "CollectButton";
-            this.CollectButton.Size = new System.Drawing.Size(500, 250);
-            this.CollectButton.TabIndex = 2;
-            this.CollectButton.Text = "Collect";
-            this.CollectButton.UseVisualStyleBackColor = false;
-            this.CollectButton.Click += new System.EventHandler(this.CollectButton_Click);
+            this.RightButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightButton.AutoSize = true;
+            this.RightButton.BackColor = System.Drawing.Color.Gray;
+            this.RightButton.Enabled = false;
+            this.RightButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RightButton.ForeColor = System.Drawing.Color.DimGray;
+            this.RightButton.Location = new System.Drawing.Point(1388, 835);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(500, 250);
+            this.RightButton.TabIndex = 2;
+            this.RightButton.Text = "Collect";
+            this.RightButton.UseVisualStyleBackColor = false;
             // 
             // Slot1
             // 
-            this.Slot1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.Slot1.BackColor = System.Drawing.Color.Black;
+            this.Slot1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Slot1.BackColor = System.Drawing.Color.White;
             this.Slot1.Enabled = false;
-            this.Slot1.Location = new System.Drawing.Point(0, 12);
+            this.Slot1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Slot1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Slot1.Location = new System.Drawing.Point(223, 143);
             this.Slot1.Name = "Slot1";
             this.Slot1.Size = new System.Drawing.Size(500, 500);
             this.Slot1.TabIndex = 3;
@@ -154,9 +138,10 @@
             // Slot2
             // 
             this.Slot2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.Slot2.BackColor = System.Drawing.Color.Black;
+            this.Slot2.BackColor = System.Drawing.Color.White;
             this.Slot2.Enabled = false;
-            this.Slot2.Location = new System.Drawing.Point(618, 12);
+            this.Slot2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Slot2.Location = new System.Drawing.Point(842, 143);
             this.Slot2.Name = "Slot2";
             this.Slot2.Size = new System.Drawing.Size(500, 500);
             this.Slot2.TabIndex = 4;
@@ -165,37 +150,67 @@
             // 
             // Slot3
             // 
-            this.Slot3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.Slot3.BackColor = System.Drawing.Color.Black;
+            this.Slot3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Slot3.BackColor = System.Drawing.Color.White;
             this.Slot3.Enabled = false;
-            this.Slot3.Location = new System.Drawing.Point(1238, 12);
+            this.Slot3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Slot3.Location = new System.Drawing.Point(1446, 143);
             this.Slot3.Name = "Slot3";
             this.Slot3.Size = new System.Drawing.Size(500, 500);
             this.Slot3.TabIndex = 5;
             this.Slot3.UseVisualStyleBackColor = false;
             this.Slot3.Click += new System.EventHandler(this.Slot3_Click);
             // 
-            // SlotBlinkTimer
+            // BlinkTimer
             // 
-            this.SlotBlinkTimer.Interval = 250;
-            this.SlotBlinkTimer.Tick += new System.EventHandler(this.SlotBlinkTimer_Tick);
+            this.BlinkTimer.Interval = 250;
+            this.BlinkTimer.Tick += new System.EventHandler(this.SlotBlinkTimer_Tick);
             // 
             // TimeoutTimer
             // 
             this.TimeoutTimer.Interval = 1000;
             this.TimeoutTimer.Tick += new System.EventHandler(this.TimeoutTimer_Tick);
             // 
+            // CenterButton
+            // 
+            this.CenterButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.CenterButton.AutoSize = true;
+            this.CenterButton.BackColor = System.Drawing.Color.Gray;
+            this.CenterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CenterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CenterButton.ForeColor = System.Drawing.Color.DimGray;
+            this.CenterButton.Location = new System.Drawing.Point(842, 835);
+            this.CenterButton.Name = "CenterButton";
+            this.CenterButton.Size = new System.Drawing.Size(500, 250);
+            this.CenterButton.TabIndex = 6;
+            this.CenterButton.Text = "Other";
+            this.CenterButton.UseVisualStyleBackColor = false;
+            // 
+            // MenuFileNew
+            // 
+            this.MenuFileNew.Name = "MenuFileNew";
+            this.MenuFileNew.Size = new System.Drawing.Size(216, 26);
+            this.MenuFileNew.Text = "New Phase";
+            this.MenuFileNew.Click += new System.EventHandler(this.MenuFileNew_Click);
+            // 
+            // MenuFileLoad
+            // 
+            this.MenuFileLoad.Name = "MenuFileLoad";
+            this.MenuFileLoad.Size = new System.Drawing.Size(216, 26);
+            this.MenuFileLoad.Text = "Load Phase";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1738, 899);
+            this.ClientSize = new System.Drawing.Size(2176, 1138);
+            this.Controls.Add(this.CenterButton);
             this.Controls.Add(this.Slot3);
             this.Controls.Add(this.Slot2);
             this.Controls.Add(this.Slot1);
-            this.Controls.Add(this.CollectButton);
-            this.Controls.Add(this.RollButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.LeftButton);
             this.Controls.Add(this.MenuStrip);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MainMenuStrip = this.MenuStrip;
@@ -204,7 +219,6 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.MenuStrip.ResumeLayout(false);
             this.MenuStrip.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -213,20 +227,20 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip MenuStrip;
-        private System.Windows.Forms.ToolStripMenuItem MenuActions;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
         private System.Windows.Forms.ToolStripMenuItem MenuConfig;
         private System.Windows.Forms.ToolStripMenuItem MenuHelp;
         private System.Windows.Forms.ToolStripMenuItem MenuExit;
-        private System.Windows.Forms.ToolStripMenuItem MenuActionsStart;
-        private System.Windows.Forms.ToolStripMenuItem MenuActionsStop;
-        private System.Windows.Forms.Button RollButton;
-        private System.Windows.Forms.Button CollectButton;
-        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button RightButton;
         private System.Windows.Forms.Button Slot1;
         private System.Windows.Forms.Button Slot2;
         private System.Windows.Forms.Button Slot3;
-        private System.Windows.Forms.Timer SlotBlinkTimer;
+        private System.Windows.Forms.Timer BlinkTimer;
         private System.Windows.Forms.Timer TimeoutTimer;
+        private System.Windows.Forms.Button CenterButton;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileNew;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileLoad;
     }
 }
 
--- Gambling Task/Form1.cs
+++ Gambling Task/Form1.cs
@@ -14,10 +14,26 @@ namespace Gambling_Task
     {
         int activeSlot = 1;
         int timeoutTime = 10;
+        Form phaseEditor = new NewPhase();
 
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void Roll()
+        {
+
+        }
+
+        private void ActivateButton(object button)
+        {
+
+        }
+
+        private void DectivateButton(object button)
+        {
+
         }
 
         private void MenuExit_Click(object sender, EventArgs e)
@@ -25,67 +41,24 @@ namespace Gambling_Task
             Application.Exit();
         }
 
-        private void MenuActionsStart_Click(object sender, EventArgs e)
-        {
-            MenuActionsStart.Enabled = false;
-            MenuActionsStop.Enabled = true;
-            RollButton.ForeColor = Color.Black;
-            RollButton.BackColor = Color.Red;
-            RollButton.Enabled = true;
-            CollectButton.ForeColor = Color.Black;
-            CollectButton.BackColor = Color.Green;
-            CollectButton.Enabled = true;
-        }
-
-        private void MenuActionsStop_Click(object sender, EventArgs e)
-        {
-            MenuActionsStop.Enabled = false;
-            MenuActionsStart.Enabled = true;
-            RollButton.ForeColor = Color.DimGray;
-            RollButton.BackColor = Color.Gray;
-            RollButton.Enabled = false;
-            CollectButton.ForeColor = Color.DimGray;
-            CollectButton.BackColor = Color.Gray;
-            CollectButton.Enabled = false;
-        }
-
         private void RollButton_Click(object sender, EventArgs e)
         {
             Slot1.BackColor = Color.Black;
             Slot2.BackColor = Color.Black;
             Slot3.BackColor = Color.Black;
-            RollButton.Enabled = false;
-            CollectButton.Enabled = false;
-            RollButton.ForeColor = Color.DimGray;
-            RollButton.BackColor = Color.Gray;
-            CollectButton.ForeColor = Color.DimGray;
-            CollectButton.BackColor = Color.Gray;
-            SlotBlinkTimer.Enabled = true;
+            LeftButton.Enabled = false;
+            RightButton.Enabled = false;
+            LeftButton.ForeColor = Color.DimGray;
+            LeftButton.BackColor = Color.Gray;
+            RightButton.ForeColor = Color.DimGray;
+            RightButton.BackColor = Color.Gray;
+            BlinkTimer.Enabled = true;
             Slot1.Enabled = true;
         }
 
         private void CollectButton_Click(object sender, EventArgs e)
         {
-            if(Slot1.BackColor == Color.White & Slot2.BackColor == Color.White & Slot3.BackColor == Color.White)
-            {
-                MessageBox.Show("Reward dispensed!");
-                CollectButton.Enabled = false;
-                CollectButton.ForeColor = Color.DimGray;
-                CollectButton.BackColor = Color.Gray;
-                Slot1.BackColor = Color.Black;
-                Slot2.BackColor = Color.Black;
-                Slot3.BackColor = Color.Black;
-            } else
-            {
-                RollButton.Enabled = false;
-                CollectButton.Enabled = false;
-                RollButton.ForeColor = Color.DimGray;
-                RollButton.BackColor = Color.Gray;
-                CollectButton.ForeColor = Color.DimGray;
-                CollectButton.BackColor = Color.Gray;
-                RollButton.Text = timeoutTime.ToString();
-                TimeoutTimer.Enabled = true;
-            }
+
         }
 
         private void SlotBlinkTimer_Tick(object sender, EventArgs e)
@@ -158,7 +131,7 @@ namespace Gambling_Task
         private void Slot3_Click(object sender, EventArgs e)
         {
             activeSlot = 1;
-            SlotBlinkTimer.Enabled = false;
+            BlinkTimer.Enabled = false;
             Slot3.Enabled = false;
 
             if (CoinFlip())
@@ -170,12 +143,12 @@ namespace Gambling_Task
                 Slot3.BackColor = Color.Black;
             }
 
-            RollButton.Enabled = true;
-            CollectButton.Enabled = true;
-            RollButton.ForeColor = Color.Black;
-            RollButton.BackColor = Color.Red;
-            CollectButton.ForeColor = Color.Black;
-            CollectButton.BackColor = Color.Green;
+            LeftButton.Enabled = true;
+            RightButton.Enabled = true;
+            LeftButton.ForeColor = Color.Black;
+            LeftButton.BackColor = Color.Red;
+            RightButton.ForeColor = Color.Black;
+            RightButton.BackColor = Color.Green;
         }
 
         private void TimeoutTimer_Tick(object sender, EventArgs e)
@@ -184,17 +157,17 @@ namespace Gambling_Task
             if(timeoutTime == 0)
             {
                 TimeoutTimer.Enabled = false;
-                RollButton.Enabled = true;
-                RollButton.ForeColor = Color.Black;
-                RollButton.BackColor = Color.Red;
-                RollButton.Text = "Roll";
+                LeftButton.Enabled = true;
+                LeftButton.ForeColor = Color.Black;
+                LeftButton.BackColor = Color.Red;
+                LeftButton.Text = "Roll";
                 timeoutTime = 10;
                 Slot1.BackColor = Color.Black;
                 Slot2.BackColor = Color.Black;
                 Slot3.BackColor = Color.Black;
             } else
             {
-                RollButton.Text = timeoutTime.ToString();
+                LeftButton.Text = timeoutTime.ToString();
             }
         }
 
@@ -209,6 +182,11 @@ namespace Gambling_Task
             {
                 return false;
             }
+        }
+
+        private void MenuFileNew_Click(object sender, EventArgs e)
+        {
+            phaseEditor.Show();
         }
     }
 }
