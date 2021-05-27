fec5d0960304537883300efce9d484ca0c0e3a9f
Benjamin Cooper <benjaminc@ksu.edu>
2021-05-27 18:22:28 -0500

    hopefully fixed sensor bug

--- Gambling Task/bin/Debug/Dispense.py
+++ Gambling Task/bin/Debug/Dispense.py
@@ -1,28 +1,41 @@
 import sys
 import time
 import board
-import digitalio
+import RPi.GPIO as GPIO
 from adafruit_motorkit import MotorKit
 
+BEAM_PIN = 17
 kit = MotorKit(i2c=board.I2C())
 
-# break beam sensor -> GPIO #5
-break_beam = digitalio.DigitalInOut(board.D5)
-break_beam.direction = digitalio.Direction.INPUT
-break_beam.pull = digitalio.Pull.UP
-
-# tracks number of pellets dispensed
+# the number of pellets dispensed this execution
 count = 0
 
+# flag indicating that the sensor has been tripped
+beamBroken = False
+
+# sets the beamBroken flag to true if the sensor beam is newly broken
+def break_beam_callback(channel):
+    global beamBroken
+    if beamBroken == False and  GPIO.input(BEAM_PIN) == False:
+        # beam is broken
+        beamBroken = True
+
+# set up break beam sensor trigger event
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(BEAM_PIN, GPIO.BOTH, callback=break_beam_callback)
+
 if len(sys.argv) != 2:
-	print("Incorrect number of arguments!")
+    print("Missing argument: count")
 else:
-    # loop until we have dispensed the correct number of pellets
-	while count < sys.argv[1]:
-        # take 5 motor steps, with a tiny break between each
-		for i in range(5):
-			kit.stepper1.onestep()
-            time.sleep(0.01)
-        # if break beam is triggered, increment count
-        if break_beam.value:
-            count += 1
+    # loop until we've dispensed the right number of pellets
+    while count < int(sys.argv[1]):
+        if beamBroken:
+          count += 1
+          beamBroken = False
+        else:
+          kit.stepper1.onestep()
+          time.sleep(0.03)
+
+kit.stepper1.release()
+GPIO.cleanup()
